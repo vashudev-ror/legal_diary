8c22a0731329dfb45275aa7cc2a6639263d0b07d
vashudev <vashudev.sharma@litera.com>
2022-08-11 13:41:21 +0530

    asdasdavdty23fedugwsx

---
+++ app/javascript/packs/application.js
@@ -0,0 +1,73 @@
+// This file is automatically compiled by Webpack, along with any other files
+// present in this directory. You're encouraged to place your actual application logic in
+// a relevant structure within app/javascript and only use these pack files to reference
+// that code so it'll be compiled.
+
+import Rails from "@rails/ujs"
+import Turbolinks from "turbolinks"
+import * as ActiveStorage from "@rails/activestorage"
+import "channels"
+import '../stylesheets/application'
+require("jquery")
+
+
+Rails.start()
+Turbolinks.start()
+ActiveStorage.start()
+global.toastr = require("toastr")
+$(document).on('ready pjax:success', function() {
+  handleActiveBase();
+  function handleActiveBase() {
+    $('.sub-menu').each(function () {
+      if ($(this).hasClass('active')) {
+        $(this).parent().prev().addClass('active');
+        $(this).parent().prev().addClass('open');
+        $(this).parent().slideDown();
+      }
+    });
+  }
+});
+
+$(function () {
+  var width = $('.nav-stacked').width();
+  $('.navbar-header').width(width);
+
+  var array_menu = [];
+  var lvl_1 = null;
+  var count = 0;
+
+  $('.sidebar-nav li').each(function (index, item) {
+    if ($(item).hasClass('dropdown-header')) {
+      lvl_1 = count++;
+      array_menu[lvl_1] = []
+    } else {
+      $(item).addClass('sub-menu sub-menu-' + lvl_1);
+    }
+  });
+
+  for (var i = 0; i <= array_menu.length; i++) {
+    $('.sub-menu-' + i).wrapAll("<div class='sub-menu-container' />");
+  }
+
+  $('.sub-menu-container').hide();
+
+  handleActiveBase();
+  function handleActiveBase() {
+    $('.sub-menu').each(function () {
+      if ($(this).hasClass('active')) {
+        $(this).parent().prev().addClass('active');
+        $(this).parent().slideDown();
+      }
+    });
+  }
+
+  $('.dropdown-header').bind('click', function () {
+    $('.dropdown-header').removeClass('open');
+    $(this).addClass('open');
+
+    $('.dropdown-header').removeClass('active');
+    $('.sub-menu-container').stop().slideUp();
+    $(this).toggleClass('active');
+    $(this).next('.sub-menu-container').stop().slideDown();
+  });
+});
